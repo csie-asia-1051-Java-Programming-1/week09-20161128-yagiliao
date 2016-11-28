4a8174280ef40631ce6ce15fb2e51496110210fb
yagiliao <105021039@live.asia.edu.tw>
2016-11-28 16:28:52 +0800

    ex

--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -4,12 +4,28 @@
  * Date: 2016/11/28
  * Author: 1050210XX 周永振老師
  */
-
+import java.util.Scanner;
 public class ex03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		boolean val=false;
+		for(int i=2;i<n;i++){
+			int sum=n%i;
+			if(sum==0){
+				System.out.print(i+" ");
+				val=true;
+			}
+		}
+		System.out.println();
+		if(val==false){
+			System.out.print("是質數");
+		}else
+			if(val==true){
+				System.out.print("不是質數");
+			}
 	}
 
 }
